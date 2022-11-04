4982dbd2acde312f4286310d61fdb306f3357140
wtxj <116582564+wtxj@users.noreply.github.com>
2022-11-04 16:37:31 +0800

    Create CamScanner.js

---
+++ CamScanner.js
@@ -0,0 +1,22 @@
+/**************************
+CamScanner 解锁部分高级特权
+
+***************************
+Quantumult X:
+
+[rewrite_local]
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body CamScanner.js
+
+[mitm]
+hostname = ap*.intsig.net
+
+***************************
+Loon:
+
+[Script]
+http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=CamScanner.js
+
+[MITM]
+hostname = ap*.intsig.net
+**************************/
+let obj = JSON["\x70\x61\x72\x73\x65"]($response["\x62\x6f\x64\x79"]);obj = {"\x64\x61\x74\x61":{"\x70\x73\x6e\x6c\x5f\x76\x69\x70\x5f\x70\x72\x6f\x70\x65\x72\x74\x79":{"\x65\x78\x70\x69\x72\x79":"\x31\x39\x32\x30\x36\x31\x34\x34\x30\x33"}}};$done({body: JSON["\x73\x74\x72\x69\x6e\x67\x69\x66\x79"](obj)});
