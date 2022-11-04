f152127032f9590381727257568636eea20b0f6d
wtxj <116582564+wtxj@users.noreply.github.com>
2022-11-04 16:39:31 +0800

    Update CamScanner.js

--- CamScanner.js
+++ CamScanner.js
@@ -5,7 +5,7 @@ CamScanner 解锁部分高级特权
 Quantumult X:
 
 [rewrite_local]
-^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body CamScanner.js
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/wtxj/wtxj/main/CamScanner.js
 
 [mitm]
 hostname = ap*.intsig.net
@@ -14,7 +14,7 @@ hostname = ap*.intsig.net
 Loon:
 
 [Script]
-http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=CamScanner.js
+http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/wtxj/wtxj/main/CamScanner.js
 
 [MITM]
 hostname = ap*.intsig.net
