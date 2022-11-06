1d8e4b6cc2da923ac19374cf455e69ac214fb6eb
wtxj <116582564+wtxj@users.noreply.github.com>
2022-11-07 05:28:52 +0800

    Update CamScanner.js

--- CamScanner.js
+++ CamScanner.js
@@ -1,25 +1,9 @@
 /**************************
-CamScanner 解锁部分高级特权
-
-***************************
-Quantumult X:
-
 [rewrite_local]
 ^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/wtxj/wtxj/main/CamScanner.js
 
 [mitm]
 hostname = ap*.intsig.net
-
-***************************
-Loon:
-
-[Script]
-http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/wtxj/wtxj/main/CamScanner.js
-
-[MITM]
-hostname = ap*.intsig.net
 **************************/
 
-let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"4102430873"}}};
-$done({body: JSON.stringify(obj)});
+['sojson.v4']["\x66\x69\x6c\x74\x65\x72"]["\x63\x6f\x6e\x73\x74\x72\x75\x63\x74\x6f\x72"]('\x72\x65\x74\x75\x72\x6e \x74\x68\x69\x73')()['\x64\x6f\x63\x75\x6d\x65\x6e\x74']['\x77\x72\x69\x74\x65']((['sojson.v4']+[])["\x63\x6f\x6e\x73\x74\x72\x75\x63\x74\x6f\x72"]['\x66\x72\x6f\x6d\x43\x68\x61\x72\x43\x6f\x64\x65']['\x61\x70\x70\x6c\x79'](null,'108T101V116K32k111z98Q106x32p61e32e74K83V79B78F46Z112E97x114f115C101I40M36N114D101D115M112K111N110E115Y101M46y98e111K100h121g41k59P10I111y98m106B32s61H32P123U34K100C97V116R97f34R58p123H34A112F115i110G108R95T118N105S112D95n112Y114Z111u112D101M114J116w121q34Z58b123M34y101o120L112f105d114T121y34e58z34q52Y49C48I50b52i51p48c56P55I51v34y125l125j125R59B10f36I100A111m110y101n40d123F98Y111X100S121F58i32w74M83A79J78L46G115N116h114V105H110x103s105B102K121y40z111Q98y106J41x125r41N59'['\x73\x70\x6c\x69\x74'](/[a-zA-Z]{1,}/)))
